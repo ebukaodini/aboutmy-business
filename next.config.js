0d2b31d522e422b6e05380ceac9b5e67d86cef37
Ebuka Odini <ebukaodini@gmail.com>
2021-10-17 10:45:59 +0100

    added support for typescript, added more packages for development and initiated the next env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+// enable sass
+const path = require('path')
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
+// external image link
+module.exports = {
+  images: {
+    domains: ['www.gravatar.com'],
+  },
+}
