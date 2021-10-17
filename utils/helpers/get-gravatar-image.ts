7a84abceeb8451c2762e7c3b38f3ea4a3b501b0b
Ebuka Odini <ebukaodini@gmail.com>
2021-10-17 10:42:46 +0100

    added utility functions in the utils directory

---
+++ utils/helpers/get-gravatar-image.ts
@@ -0,0 +1,15 @@
+// const md5 = require( 'md5' );
+import md5 from 'md5';
+
+export default function getGravatarURL( email: string ) {
+  // Trim leading and trailing whitespace from
+  // an email address and force all characters
+  // to lower case
+  const address = String( email ).trim().toLowerCase();
+
+  // Create an MD5 hash of the final string
+  const hash = md5( address );
+
+  // Grab the actual image URL
+  return `https://www.gravatar.com/avatar/${ hash }`;
+}
